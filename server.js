9a9f1d05d34bb9c9a21a5ef7e33698ee235bfad4
Mario Habib <mario.h.habib@gmail.com>
2022-06-27 01:24:35 +0200

    make it better

--- server.js
+++ server.js
@@ -645,6 +645,6 @@ app.get('/logout',  (request, response, next) => {
 app.get('*', function(request, response){
 	response.render('notfound');
   });
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}...`);
